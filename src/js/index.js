553975211c209cce6b2ce234db81e54e1bf05c1e
brianokanga <briannokanga@gmail.com>
2020-08-26 06:10:33 +0300

    add app architecuture and how imports and exports work

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
 // Global app controller
-import num from './test';
-const x = 23;
-console.log(
-	`I imported ${num} from another module called test.js! Variable x is ${x}`
+import str from './models/Search';
+
+// import {add as a, multiply as m, ID} from './views/Searchview';
+import * as Searchview from './views/Searchview';
+
+// console.log (
+//   `Using imported functions! ${a (ID, 2)} and multiply ${m (3, 5)}. ${str} `
+// );
+
+console.log (
+  `Using imported functions! ${Searchview.add (Searchview.ID, 2)} and multiply ${Searchview.multiply (3, 5)}. ${str} `
 );
