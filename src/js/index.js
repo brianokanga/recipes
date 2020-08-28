61deeece673c38db4b6301a796c9a040370cd1e7
Brian Okanga <briannokanga@gmail.com>
2020-08-28 12:23:27 +0300

    Added babel files amd configurations

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+async function getResults(query) {
+	const proxy = 'https://cors-anywhere.herokuapp.com/';
+	const key = '';
+
+	try {
+		const res = await axios(
+			`${proxy}https://recipesapi.herokuapp.com/api/search?key=${key}&q=${query}`
+		);
+		const recipes = res.data.recipes;
+		console.log(recipes);
+	} catch (error) {
+		alert(error);
+	}
+}
+
+getResults('pizza');
